73cc6e25e846c8715640eee86b85c49e44fb33cf
Karadar1 <lazau.tudor@yahoo.com>
2025-04-17 18:59:16 +0300

    flask setup with models and mysql integration

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from dotenv import load_dotenv
+from config import Config
+from models import db
+from models.association import recipe_category
+from models.category import Category
+from models.ingredient import Ingredient
+from models.recipe import Recipe
+
+
+
+load_dotenv()
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+@app.route('/')
+def helloWorld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    with app.app_context():
+        # db.create_all()
+        db.drop_all()
+
+    app.run(debug=True)

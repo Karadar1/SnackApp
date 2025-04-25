c2d02c557b9e981f2e4b2062a2621c6e1ac146b2
Karadar1 <lazau.tudor@yahoo.com>
2025-04-25 16:56:22 +0300

    change recipe duration to string

--- api/models/recipe.py
+++ api/models/recipe.py
@@ -7,7 +7,7 @@ class Recipe(db.Model):
     __tablename__ = 'recipe'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.String(50), nullable=False)
     pictures = db.Column(db.String(100), nullable=False)
     instructions = db.Column(db.String(100), nullable=False)
 
